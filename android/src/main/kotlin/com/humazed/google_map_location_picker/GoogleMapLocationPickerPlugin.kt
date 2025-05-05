06b80ab2dab42683dab9ea27796b384181de5c73
hady mohamed <hadykamel3@gmail.com>
2025-05-05 14:47:11 +0300

    fix latest issue

--- android/src/main/kotlin/com/humazed/google_map_location_picker/GoogleMapLocationPickerPlugin.kt
+++ android/src/main/kotlin/com/humazed/google_map_location_picker/GoogleMapLocationPickerPlugin.kt
@@ -1,31 +1,47 @@
 package com.humazed.google_map_location_picker
 
 import android.app.Activity
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
-import androidx.annotation.UiThread
+import android.content.Context
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
-import java.math.BigInteger
-import java.security.MessageDigest
 
+class GoogleMapLocationPickerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
-class GoogleMapLocationPickerPlugin(act: Activity?) : MethodCallHandler {
-    var activity: Activity? = act
+    private lateinit var channel: MethodChannel
+    private var activity: Activity? = null
 
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "google_map_location_picker")
-            channel.setMethodCallHandler(GoogleMapLocationPickerPlugin(registrar.activity()))
-        }
+    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "google_map_location_picker")
+        channel.setMethodCallHandler(this)
     }
 
-    @UiThread
-    override fun onMethodCall(call: MethodCall, result: Result) {
-    
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        // TODO: handle method calls
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
     }
 }
